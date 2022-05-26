c71c45ab2bd1fea8823ae3804234dcdf7089fa0d
ff05 <frozenflame2005@mailbox.org>
2022-05-26 21:18:37 +0200

    first commit

--- main.py
+++ main.py
@@ -16,49 +16,49 @@ def createCalendar(plan,name):
         cal.add_component(game)
     return cal
 
+def main()
+    team='Gelsenkirchen Devils'
+    url = "https://afcvnrw.de/wp-content/themes/afcv/ajax/games_spielplan.php"
 
-team='Gelsenkirchen Devils'
-url = "https://afcvnrw.de/wp-content/themes/afcv/ajax/games_spielplan.php"
-
-headers = CaseInsensitiveDict()
-headers["authority"] = "afcvnrw.de"
-headers["accept"] = "*/*"
-headers["accept-language"] = "en-US,en;q=0.9"
-headers["content-type"] = "application/x-www-form-urlencoded; charset=UTF-8"
-headers["origin"] = "https://afcvnrw.de"
-headers["referer"] = "https://afcvnrw.de/ergebnisse/spielplan/"
-headers["sec-ch-ua"] = '" Not A;Brand";v="99", "Chromium";v="101"'
-headers["sec-ch-ua-mobile"] = "?0"
-headers["sec-ch-ua-platform"] = '"Linux"'
-headers["sec-fetch-dest"] = "empty"
-headers["sec-fetch-mode"] = "cors"
-headers["sec-fetch-site"] = "same-origin"
-headers["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
-headers["x-requested-with"] = "XMLHttpRequest"
-data = "league=493"
-page = requests.post(url, headers=headers, data=data)
-soup=bs4.BeautifulSoup(page.content, 'lxml')
-ligaplanhtm=soup.findAll("div",{"class":"game_result spielplan"})
-ligaplan=[]
-teamplan=[]
-for gamehtm in ligaplanhtm:
-    game={}
-    kickoff=datetime.datetime.strptime((gamehtm.find("div",{"class":"kickoff"}).text),'%d.%m.%y um %H:%M Uhr')
-    game['kickoff']=kickoff.strftime("%m/%d/%Y, %H:%M:%S")
-    game['hometeam']=(gamehtm.find("div",{"class":"team1"}).text)
-    game['guestteam']=(gamehtm.find("div",{"class":"team2"}).text)
-    ligaplan.append(game)
-    if game['hometeam'] == team or game['guestteam'] == team:
-        teamplan.append(game)
+    headers = CaseInsensitiveDict()
+    headers["authority"] = "afcvnrw.de"
+    headers["accept"] = "*/*"
+    headers["accept-language"] = "en-US,en;q=0.9"
+    headers["content-type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+    headers["origin"] = "https://afcvnrw.de"
+    headers["referer"] = "https://afcvnrw.de/ergebnisse/spielplan/"
+    headers["sec-ch-ua"] = '" Not A;Brand";v="99", "Chromium";v="101"'
+    headers["sec-ch-ua-mobile"] = "?0"
+    headers["sec-ch-ua-platform"] = '"Linux"'
+    headers["sec-fetch-dest"] = "empty"
+    headers["sec-fetch-mode"] = "cors"
+    headers["sec-fetch-site"] = "same-origin"
+    headers["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+    headers["x-requested-with"] = "XMLHttpRequest"
+    data = "league=493"
+    page = requests.post(url, headers=headers, data=data)
+    soup=bs4.BeautifulSoup(page.content, 'lxml')
+    ligaplanhtm=soup.findAll("div",{"class":"game_result spielplan"})
+    ligaplan=[]
+    teamplan=[]
+    for gamehtm in ligaplanhtm:
+        game={}
+        kickoff=datetime.datetime.strptime((gamehtm.find("div",{"class":"kickoff"}).text),'%d.%m.%y um %H:%M Uhr')
+        game['kickoff']=kickoff.strftime("%m/%d/%Y, %H:%M:%S")
+        game['hometeam']=(gamehtm.find("div",{"class":"team1"}).text)
+        game['guestteam']=(gamehtm.find("div",{"class":"team2"}).text)
+        ligaplan.append(game)
+        if game['hometeam'] == team or game['guestteam'] == team:
+            teamplan.append(game)
 
 
-teamcal=createCalendar(teamplan,team)
-leaguecal=createCalendar(ligaplan, "league")
+    teamcal=createCalendar(teamplan,team)
+    leaguecal=createCalendar(ligaplan, "league")
 
-team +=".ics"
-f = open(team, 'wb')
-f.write(teamcal.to_ical())
-f.close()
-f = open("liga.ics", 'wb')
-f.write(leaguecal.to_ical())
-f.close()
+    team +=".ics"
+    f = open(team, 'wb')
+    f.write(teamcal.to_ical())
+    f.close()
+    f = open("liga.ics", 'wb')
+    f.write(leaguecal.to_ical())
+    f.close()
