7f5d4c93ec36b0537a49afde720850c99089fb35
Manuel Hartung <mhartung@mailbox.org>
2023-06-06 20:50:35 +0200

    multi team and league integration

--- main.py
+++ main.py
@@ -17,18 +17,17 @@ def createCalendar(plan, name):
     kickoff = datetime.datetime.strptime(
       "%s-+0200" % gameday["kickoff"], "%m/%d/%Y, %H:%M:%S-%z"
     )
-    location = '%s, https://www.google.de/maps/place/%s' % (gameday["stadium"], quote(gameday["stadium"]))
+    location = f'{gameday["stadium"]}\nhttps://www.google.de/maps/place/{quote(gameday["stadium"])}'
     game.add("summary", "%s vs %s" % (gameday["hometeam"], gameday["guestteam"]))
     game.add("dtstart", kickoff)
     game.add("dtend", kickoff + datetime.timedelta(hours=3))
     game.add("location", location)
     game.add("description", gameday["description"])
-
     cal.add_component(game)
   return cal
 
 
-def main(id, leaguename):
+def main(league_id, leaguename):
   url = "https://afcvnrw.de/wp-content/themes/afcv/ajax/games_spielplan.php"
   headers = CaseInsensitiveDict()
   headers["authority"] = "afcvnrw.de"
@@ -43,12 +42,10 @@ def main(id, leaguename):
   headers["sec-fetch-dest"] = "empty"
   headers["sec-fetch-mode"] = "cors"
   headers["sec-fetch-site"] = "same-origin"
-  headers[
-    "user-agent"
-  ] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+  headers["user-agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
   headers["x-requested-with"] = "XMLHttpRequest"
   # data = "league=493"
-  data = "league=%s" % str(id)
+  data = "league=%s" % str(league_id)
   page = requests.post(url, headers=headers, data=data)
   soup = bs4.BeautifulSoup(page.content, "lxml")
   spielplan = soup.findAll("div", {"class": "game_result spielplan"})
@@ -67,28 +64,40 @@ def main(id, leaguename):
       game["hometeam"] = spielplan[i].find("div", {"class": "team1"}).text
       game["guestteam"] = spielplan[i].find("div", {"class": "team2"}).text
       game["stadium"] = gameinfo[i].find("div", {"class": "game_stadium"}).text[7:]
+      ergebnis = spielplan[i].find("div",{"class":"resultbox"}).text
+      description = [f'Ergebnis: {ergebnis}']
       for team in config.SAUSAGE_BAD:
         if team in game["hometeam"]:
-          game["description"] = "schlechte Bratwurst"
+          description.append("schlechte Bratwurst")
       for team in config.SAUSAGE_GOOD:
         if team in game["hometeam"]:
-          game["description"] = "Gute Bratwurst"
+          description.append("Gute Bratwurst")
       if "description" not in game:
-        game["description"] = "Keine Bratwurst Information"
+        description.append("Keine Bratwurst Information")
+      for k in config.TEAM_COMMENTS:
+        if game["hometeam"] == k:
+          description.append(config.TEAM_COMMENTS[k])
+          break
       ligaplan.append(game)
-      if game["hometeam"] == config.TEAM or game["guestteam"] == config.TEAM:
+      if game["hometeam"] in config.TEAMS or game["guestteam"] in config.TEAMS:
         teamplan.append(game)
+      game["description"] = "\n".join(description)
       i+=1
     except IndexError:
       break
-
-  teamcal = createCalendar(teamplan, config.TEAM)
+  
+  if len(teamplan) > 1:
+    if teamplan[0]["hometeam"] in config.TEAMS:
+      teamname = teamplan[0]["hometeam"]
+    else:
+      teamname = teamplan[0]["guestteam"]
+    teamname = teamname.replace('/','-').replace(' ','_').replace('Ä','Ae').replace('Ö','Oe').replace('Ü','Ue').replace('ä','ae').replace('ö','oe').replace('ü','ue').replace('ß','ss')
+    teamcal = createCalendar(teamplan, teamname)
+    teamcalfile = "%s.ics" % teamname
+    f = open(teamcalfile, "wb")
+    f.write(teamcal.to_ical())
+    f.close()
   leaguecal = createCalendar(ligaplan, "league")
-
-  teamcalfile = "%s.ics" % config.TEAM
-  f = open(teamcalfile, "wb")
-  f.write(teamcal.to_ical())
-  f.close()
   f = open("%s.ics" % leaguename, "wb")
   f.write(leaguecal.to_ical())
   f.close()
---
+++ sample_config.py
@@ -0,0 +1,17 @@
+TEAMS = ["Gelsenkirchen Devils","Mülheim Shamrocks Damen"]
+SAUSAGE_BAD = ["Kevelaer Kings"]
+SAUSAGE_GOOD = ["Gelsenkirchen Devils", "Münster Blackhawks", "Herne Black Barons"]
+TEAM_COMMENTS = {
+    "Herne Black Barons": "Keine Trommeln"
+}
+LEAGUE_IDS=[
+    {
+        "id":530,
+        "name":"verbandsliga_west"
+    },{
+        "id":552,
+        "name":"U13_NRW-LIGA_Westfalen"
+    },{
+        "id":535,
+        "name": "Damenbundesliga_2-West"
+    }]
